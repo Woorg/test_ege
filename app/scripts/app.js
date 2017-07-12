1a7e4dd49c0191099dec43bdd62b723bda37c542
Igor Gorlov <gorlov35@gmail.com>
2017-07-12 19:04:48 +0300

    add map

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,62 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 
-$(() => {
+(function ($) {
 	svg4everybody();
-});
+
+	$(function () {
+		let $checkbox = $('.subjects__checkboxes .wpcf7-list-item input');
+		let $checkClass = 'subjects__checked';
+
+
+		$checkbox.on('click' , function (e) {
+			if( this.checked ) {
+				console.log('true');
+				$(this).parent().parent().addClass($checkClass);
+				$(this).hover().css();
+			} else {
+				$(this).parent().parent().removeClass($checkClass);
+			}
+		});
+
+
+		// map
+
+
+
+		ymaps.ready(function () {
+			var myMap = new ymaps.Map('map', {
+					center: [59.940875, 30.356795],
+					zoom: 17,
+					controls: ['']
+				}, {
+				});
+
+				myPlacemark = new ymaps.Placemark([59.940875, 30.356795] , {
+					// hintContent: 'Собственный значок метки',
+					// balloonContent: 'Это красивая метка'
+				}, {
+					// Опции.
+					// Необходимо указать данный тип макета.
+					iconLayout: 'default#image',
+					// Своё изображение иконки метки.
+					iconImageHref: '../images/general/marker.png',
+					// Размеры метки.
+					iconImageSize: [40, 69],
+					// Смещение левого верхнего угла иконки относительно
+					// её "ножки" (точки привязки).
+					iconImageOffset: [-5, -38]
+				}),
+
+
+			myMap.geoObjects
+				.add(myPlacemark)
+		});
+
+
+
+	});
+
+
+})(jQuery);
+
